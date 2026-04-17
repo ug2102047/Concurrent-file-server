68127ca82d9264d85300ec9a4322a9d965bb1dbf
Puspita Baidya <ug2102047@cse.pstu.ac.bd>
2026-04-17 20:58:34 +0600

    Update run.py

--- run.py
+++ run.py
@@ -2,7 +2,7 @@ from app.server.file_server import start_file_server
 from app.web.routes import start_web
 import threading
 
-
+ 
 def main():
 	server_thread = threading.Thread(target=start_file_server, daemon=True)
 	server_thread.start()
